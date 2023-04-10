47ec402a80cdd0fbc2819351b50dbb3d09dcd80e
Aamir <aamirnorth@gmail.com>
2023-04-10 09:42:40 +0400

    images fixed

--- main.js
+++ main.js
@@ -270,6 +270,29 @@ splide5.mount();
 
 
 
+const simpleParallax = (elem, modifier) => {
+    let paras = document.getElementsByClassName(elem);
+    for (let i = 0; i < paras.length; i++) {
+      paras[i].setAttribute(
+        "style",
+        "background-repeat: no-repeat; background-attachment: fixed; background-size: cover;background-position: center center;"
+      );
+    }
+    const sp = () => {
+      for (let i = 0; i < paras.length; i++) {
+        let x = paras[i].getBoundingClientRect().top / modifier;
+        let y = Math.round(x * 100) / 100;
+        paras[i].style.backgroundPosition = "center " + y + "px";
+      }
+      requestAnimationFrame(sp);
+    };
+    requestAnimationFrame(sp);
+  };
+  
+  simpleParallax("para", 10);
+
+
+
 function ready() {
     gsap.set(beforeLoading, {display: 'none'})    
     gsap.set(siteWrapper, {display: 'block'})    
